d82395109071c934c366031414fb8950a2c20924
Danny Ben Shitrit <dannyb@gmail.com>
2014-10-16 17:47:01 +0300

    Added err panic

--- travis_test.go
+++ travis_test.go
@@ -16,16 +16,18 @@ func Example() {
 	// fmt.Println(travistest.Exists(handler.Filename(key)))
 
 	fi, err := os.Stat(handler.Filename(key))
-	fmt.Println("1 --->", fi.Mode())
-
-	file := "./travis.go"
-	fi, err = os.Stat(file)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("2 --->", fi.Mode())
+	fmt.Println("1 --->", fi.Mode())
+
+	// file := "./travis.go"
+	// fi, err = os.Stat(file)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("2 --->", fi.Mode())
 
 	// Output:
-	// HELLO
-	// true <nil>
+	// -----> RESULT
 }
