df1fed52c424a684602a4dfce515a524e6c50e59
Danny Ben Shitrit <dannyb@gmail.com>
2014-10-16 17:12:42 +0300

    Ignoring cache

--- travis_test.go
+++ travis_test.go
@@ -2,19 +2,15 @@ package travistest_test
 
 import (
 	"fmt"
-	"github.com/DannyBen/filecache"
 	"github.com/DannyBen/travistest"
 )
 
 func Example() {
-	// travistest.CreateFile("file")
-	// yes, err := travistest.Exists("file")
-	// fmt.Println(yes, err)
-
-	handler := filecache.Handler{"./cache", 1}
+	handler := travistest.Handler{"./cache", 1}
 	data := []byte("HELLO")
-	handler.Set("testkey", data)
-	r := handler.Get("testkey")
+	key := "key"
+	handler.Set(key, data)
+	r := handler.Get(key)
 	fmt.Println(string(r))
 	fmt.Println(travistest.Exists(handler.Filename("testkey")))
 
