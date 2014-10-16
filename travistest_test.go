6c6e8761ed3678a3e43e3b4f7bc81f9378307d73
Danny Ben Shitrit <dannyb@gmail.com>
2014-10-16 17:03:05 +0300

    Filecache test

--- travistest_test.go
+++ travistest_test.go
@@ -2,13 +2,23 @@ package travistest_test
 
 import (
 	"fmt"
+	"github.com/DannyBen/filecache"
 	"github.com/DannyBen/travistest"
 )
 
 func Example() {
-	travistest.CreateFile("file")
-	yes, err := travistest.Exists("file")
-	fmt.Println(yes, err)
+	// travistest.CreateFile("file")
+	// yes, err := travistest.Exists("file")
+	// fmt.Println(yes, err)
+
+	handler := filecache.Handler{"./cache", 1}
+	data := []byte("HELLO")
+	handler.Set("testkey", data)
+	r := handler.Get("testkey")
+	fmt.Println(string(r))
+	fmt.Println(travistest.Exists(handler.Filename("testkey")))
+
 	// Output:
+	// HELLO
 	// true <nil>
 }
