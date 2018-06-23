51cf812304fccb10b2131a3c5c4fddcb0cb3fa6b
Danny Ben Shitrit <db@dannyben.com>
2018-06-23 08:40:26 +0000

    animated-piece

--- test.rb
+++ test.rb
@@ -2,10 +2,10 @@ require 'yaml'
 
 
 versions = {
-  'python'    => `python --version 2>&1`.strip,
-  'pip'       => `pip --version 2>&1`.strip,
-  'pip3'      => `pip3 --version 2>&1`.strip,
-  'ruby'      => `ruby -v 2>&1`.strip,
+  'python'     => `python --version 2>&1`.strip,
+  'pip'        => `pip --version 2>&1`.strip,
+  'ruby'       => `ruby -v 2>&1`.strip,
+  'codenamize' => `codenamize --version`.strip
 }
 
 puts versions.to_yaml
