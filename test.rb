201d32ab8230d15fa6835594954a22280d27a818
Danny Ben Shitrit <db@dannyben.com>
2018-06-23 08:16:26 +0000

    black-desire

--- test.rb
+++ test.rb
@@ -3,10 +3,8 @@ require 'yaml'
 
 versions = {
   'python'    => `python --version 2>&1`.strip,
-  # 'python3.4' => `python --version 2>&1`.strip,
   'pip'       => `pip --version 2>&1`.strip,
   'pip3'      => `pip3 --version 2>&1`.strip,
-  # 'pip3.4'    => `pip3.4 --version 2>&1`.strip,
   'ruby'      => `ruby -v 2>&1`.strip,
 }
 
