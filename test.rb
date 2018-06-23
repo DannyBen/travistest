46e10db76b2b3ca7001d4d43799a4fd30a154b42
Danny Ben Shitrit <db@dannyben.com>
2018-06-23 08:01:40 +0000

    zany-kid

--- test.rb
+++ test.rb
@@ -1,3 +1,11 @@
+require 'yaml'
 
 
-puts "works"
+versions = {
+  'python' => `python --version 2>&1`.strip,
+  'pip' => `pip --version 2>&1`.strip,
+  'pip3' => `pip3 --version 2>&1`.strip,
+  'ruby' => `ruby -v 2>&1`.strip,
+}
+
+puts versions.to_yaml
